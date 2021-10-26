f78cac3a041fbb1117ed5172dd16777c73a3e558
RS Finance Labs <52669170+RS-Finance@users.noreply.github.com>
2021-10-26 04:13:27 -0700

    totalSupply added

---
+++ api/totalSupply.ts
@@ -0,0 +1,17 @@
+import { NowRequest, NowResponse } from "@vercel/node";
+import { getBurnedSupply, getTotalSupply, getLockedSupply } from "../utils/supply";
+
+export default async (req: NowRequest, res: NowResponse): Promise<void> => {
+  let totalSupply = await getTotalSupply();
+  totalSupply = totalSupply.div(1e18);
+
+  let burnedSupply = await getBurnedSupply();
+  burnedSupply = burnedSupply.div(1e18);
+
+  let lockedSupply = await getLockedSupply();
+  lockedSupply = lockedSupply.div(1e18);
+
+  res.json({
+    totalSupply: totalSupply.minus(burnedSupply).toNumber(),
+  });
+};
